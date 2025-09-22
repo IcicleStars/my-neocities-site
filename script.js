a050e8e078b16cbd792cb49f51f315f1d2926824
IcicleStars <ice.randellesalvador@gmail.com>
2025-09-21 20:53:49 -0700

    Added close option and updated animations

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-const books = document.querySelectorAll('.book.highlight');
-    const contents = document.querySelectorAll('.content');
+// Get HTML Elements
+const books = document.querySelectorAll('.book.interactive');
+const contents = document.querySelectorAll('.content');
+const closeButtons = document.querySelectorAll('.close-btn');
 
-    books.forEach(book => {
-      book.addEventListener('click', () => {
+// BOOK CLICK FUNCTIONALITY
+books.forEach(book => {
+    book.addEventListener('click', () => {
         // Reset others
         books.forEach(b => b.classList.remove('active'));
         contents.forEach(c => c.classList.remove('active'));
@@ -13,5 +16,24 @@ const books = document.querySelectorAll('.book.highlight');
         // Show associated content
         const targetId = book.getAttribute('data-content');
         document.getElementById(targetId).classList.add('active');
-      });
     });
+});
+
+// CLOSE BUTTON FUNCITONALITY
+closeButtons.forEach(button => {  
+    button.addEventListener('click', () => { 
+
+        // Get parent content and deactivate it
+        const content = button.closest('.content');
+        if (content) {
+            content.classList.remove('active');
+        }
+
+        // Deactivate active book
+        const activeBook = document.querySelector('.book.interactive.active');
+        if (activeBook) {
+            activeBook.classList.remove('active');
+        }
+
+    });
+});
